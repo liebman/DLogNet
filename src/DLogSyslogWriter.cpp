5025d38f386a70dcf10593c537e2a8ab1a37d93b
Chris Liebman <chris.l@taboola.com>
2019-09-07 17:32:36 -0700

    add optional delay after each log message to keep esp8266 from dropping messages update version to 1.0.2

--- src/DLogSyslogWriter.cpp
+++ src/DLogSyslogWriter.cpp
@@ -7,6 +7,10 @@
 
 #include <DLogSyslogWriter.h>
 
+#ifndef DLOG_SYSLOG_DELAY
+#define DLOG_SYSLOG_DELAY 0
+#endif
+
 DLogSyslogWriter::DLogSyslogWriter(const char* host, uint16_t port, const char* devicename, const char* appname, uint16_t defpri) :
         _syslog(_udp, host, port, devicename, appname, defpri, SYSLOG_PROTO_BSD)
 {
@@ -19,4 +23,7 @@ DLogSyslogWriter::~DLogSyslogWriter()
 void DLogSyslogWriter::write(const char* message)
 {
     _syslog.log(LOG_USER | LOG_INFO, message);
+#if DLOG_SYSLOG_DELAY > 0
+    delay(DLOG_SYSLOG_DELAY);
+#endif
 }
